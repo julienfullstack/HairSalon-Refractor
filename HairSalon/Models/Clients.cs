3aabd86fc4baf15dd73e418543487a8480332a3a
Julien L <julienlenaz96@gmail.com>
2023-10-17 22:39:19 -0400

    Added validation

--- HairSalon/Models/Clients.cs
+++ HairSalon/Models/Clients.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HairSalon.Models
 {
     public class Client
     {
         public int ClientId { get; set; }
-        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field can't be left empty")]
+        public string? Name { get; set; }
         public int StylistId { get; set; }
         public virtual Stylist Stylist { get; set; }
         public List<ClientStylist> JoinEntities { get;}
--- HairSalon/Models/Stylists.cs
+++ HairSalon/Models/Stylists.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HairSalon.Models
 {
     public class Stylist
     {
         public int StylistId { get; set; }
-        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "This field can't be left empty")]
+        public string? Name { get; set; }
         
          public List<Client> Clients{ get; set; }
         
