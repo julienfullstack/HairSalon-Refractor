837c9fd12d0086b3f71167a183f43e3d57479ff8
Julien L <julienlenaz96@gmail.com>
2023-10-17 22:31:57 -0400

    Added Splash page

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly HairSalonContext _db;
+
+		public HomeController(HairSalonContext db)
+		{
+			_db = db;
+		}
+
 		[HttpGet("/")]
 		public ActionResult Index()
 		{
-			return View();
+			Stylist[] stylists = _db.Stylists.ToArray();
+			Client[] clients = _db.Clients.ToArray();
+			Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+			model.Add("stylists", stylists);
+			model.Add("clients", clients);
+			return View(model);
 		}
 	}
 }
